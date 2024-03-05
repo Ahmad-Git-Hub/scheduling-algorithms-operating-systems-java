d7ab7d8986af32269ad695a01b18c6904da0f21a
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-03-05 11:50:33 +0300

    Change class name

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        ClsProcess process = new ClsProcess();
+        Process process = new Process();
         process.displayMenu();
 
     }
